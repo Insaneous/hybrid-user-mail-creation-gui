587ed43bf27a0fae8dbf6976c11a8d4d7e629389
Erik Stopnitskiy <e.stopnitskiy@centrum-group.uz>
2025-12-09 10:41:12 +0500

    preload fix

--- preload.js
+++ preload.js
@@ -22,28 +22,3 @@ contextBridge.exposeInMainWorld('api', {
   deleteUserFromCSV: (filePath, index) => ipcRenderer.invoke('csv:deleteUser', { filePath, index }),
   onStatus: (cb) => ipcRenderer.on('deploy:status', (_, data) => cb(data))
 });
-
-
- $sec = ConvertTo-SecureString "AfsxP6cv07" -AsPlainText -Force 
-      $cred = New-Object System.Management.Automation.PSCredential("centrum-air\az.ruziev-su", $sec) 
-        # Получаем группы текущего пользователя на домене
-        $groupDns = Invoke-Command -ComputerName TASVDC04 -Credential $cred -ScriptBlock {
-            (Get-ADUser $env:USERNAME -Properties memberOf).memberOf
-        }
-
-        # Приводим к строке для простого поиска
-        $groups = $groupDns -join ";"
-
-        # Группы, которые должны присутствовать
-        $required1 = "air.tas.Mail.Recipents"
-        $required2 = "AIR.TAS.Local.Admins"
-
-        # Проверка вхождения обеих групп (по CN, без зависимости от DN)
-        $hasGroup1 = $groups -match $required1
-        $hasGroup2 = $groups -match $required2
-
-        if ($hasGroup1 -and $hasGroup2) {
-          Write-Output "OK"
-        } else {
-          Write-Output "NOADMIN"
-        }
